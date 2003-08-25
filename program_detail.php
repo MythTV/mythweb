a01ff79b43fce5ece37d1a93af0d6f0e3c97449f
Chris Petersen <cpetersen@mythtv.org>
2003-08-25 05:31:22 +0000

    minor change to program_detail.php (thanks to Geoffrey Hausheer <ou401cru02*sneakemail.com>), along with some general cleanup to the same file
    git-svn-id: http://svn.mythtv.org/svn/trunk@2116 7dbf422c-18fa-0310-86e9-fd20926502f2

--- program_detail.php
+++ program_detail.php
@@ -1,6 +1,6 @@
 <?
 /***                                                                        ***\
-	program_detail.php                      Last Updated: 2003.07.30 (xris)
+	program_detail.php                      Last Updated: 2003.08.24 (xris)
 
 	This file is part of MythWeb, a php-based interface for MythTV.
 	See README and LICENSE for details.
@@ -25,30 +25,15 @@
 
 // The user tried to update the recording settings - update the database and the variable in memory
 	if (isset($_GET['save'])) {
-		if (isset($_GET['profile'])) {
-			$this_program->profile=$_GET['profile'];
-		}
-		if (isset($_GET['rank'])) {
-			$this_program->rank=$_GET['rank'];
-		}
-		if ((isset($_GET['recorddups']))&&($_GET['recorddups'] == "on")) {
-			$this_program->recorddups=1;
-		}else{
-			$this_program->recorddups=0;
-		}
-		if ((isset($_GET['autoexpire']))&&($_GET['autoexpire'] == "on")) {
-			$this_program->autoexpire=1;
-		}else{
-			$this_program->autoexpire=0;
-		}
-		if (isset($_GET['maxepisodes'])) {
+		if (isset($_GET['profile']))
+			$this_program->profile = $_GET['profile'];
+		if (isset($_GET['rank']))
+			$this_program->rank = $_GET['rank'];
+		if (isset($_GET['maxepisodes']))
 			$this_program->maxepisodes=$_GET['maxepisodes'];
-		}
-		if ((isset($_GET['maxnewest']))&&($_GET['maxnewest'] == "on")) {
-			$this_program->maxnewest=1;
-		}else{
-			$this_program->maxnewest=0;
-		}
+		$this_program->recorddups = (isset($_GET['recorddups']) && $_GET['recorddups'] == "on") ? 1 : 0;
+		$this_program->autoexpire = (isset($_GET['autoexpire']) && $_GET['autoexpire'] == "on") ? 1 : 0;
+		$this_program->maxnewest  = (isset($_GET['maxnewest']) && $_GET['maxnewest'] == "on")   ? 1 : 0;
 	// Update
 		switch ($_GET['record']) {
 			case 'always':
@@ -70,22 +55,25 @@
 			default:
 				$this_program->record_never();
 		}
-	}else{
+	}
+	elseif (!$this_program->will_record) {
 		//Load default settings for rank, autoexpire etc
-		$rankresult = mysql_query("SELECT rank from channel where chanid=".$this_program->chanid);
-		while($row=mysql_fetch_assoc($rankresult)){
-			$this_program->rank=$row['rank'];
+		$rankresult = mysql_query("SELECT rank from channel where chanid=".escape($this_program->chanid));
+		while ($row = mysql_fetch_assoc($rankresult)) {
+			$this_program->rank = $row['rank'];
 		}
 		$autoexpire = mysql_query("SELECT data from settings where value='AutoExpireDefault'");
-		while($row=mysql_fetch_assoc($autoexpire)){
-			$this_program->autoexpire=$row['data'];
+		while ($row = mysql_fetch_assoc($autoexpire)) {
+			$this_program->autoexpire = $row['data'];
 		}
 	}
 
-$profileresult = mysql_query("select id, name from recordingprofiles");
-while($row=mysql_fetch_assoc($profileresult)){
-	$Profiles[]=$row;
-}
+// Load the recording profiles
+	$Profiles = array();
+	$profileresult = mysql_query("select id, name from recordingprofiles");
+	while ($row = mysql_fetch_assoc($profileresult)){
+		$Profiles[] = $row;
+	}
 
 // Load the class for this page
 	require_once theme_dir."program_detail.php";
