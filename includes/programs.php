61c669f04f8325666426ec13464867bf91bcea75
Chris Petersen <cpetersen@mythtv.org>
2003-10-20 01:45:31 +0000

    wordwrap program descriptions
    git-svn-id: http://svn.mythtv.org/svn/trunk@2416 7dbf422c-18fa-0310-86e9-fd20926502f2

--- includes/programs.php
+++ includes/programs.php
@@ -241,7 +241,7 @@ class Program {
 		// Load the remaining info we got from mythbackend
 			$this->title       = $program_data[0];					# program name/title
 			$this->subtitle    = $program_data[1];					# episode name
-			$this->description = $program_data[2];					# episode description
+			$this->description = nl2br(wordwrap($program_data[2], 50));	# episode description
 			$this->category    = $program_data[3];					#
 			#$channum           = $program_data[5];					# channel number
 			#$callsign          = $program_data[6];					# callsign (eg. FOOD or SCIFI)
@@ -264,7 +264,7 @@ class Program {
 			$this->endtime         = $program_data['endtime_unix'];
 			$this->title           = $program_data['title'];
 			$this->subtitle        = $program_data['subtitle'];
-			$this->description     = $program_data['description'];
+			$this->description     = nl2br(wordwrap($program_data['description'], 50));
 			$this->category        = $program_data['category']     ? $program_data['category']       : 'Unknown';
 			$this->category_type   = $program_data['category_type'] ? $program_data['category_type'] : 'Unknown';
 			$this->airdate         = $program_data['airdate'];
