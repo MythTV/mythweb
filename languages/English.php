d29189734353f4b3649f4a929e9c41c4d22b0e39
Robert Kulagowski <rkulagowski@mythtv.org>
2004-03-01 17:56:32 +0000

    Update protocol version; don't think there are any major things that break right now.  Remove extra punctuation.
    git-svn-id: http://svn.mythtv.org/svn/trunk@3319 7dbf422c-18fa-0310-86e9-fd20926502f2

--- languages/English.php
+++ languages/English.php
@@ -199,7 +199,7 @@ define ('_LANG_RECTYPE_FINDONE', 'FindOne');
 
 define ('_LANG_RECTYPE_LONG_ONCE',          'Record only this showing.');
 define ('_LANG_RECTYPE_LONG_DAILY',         'Record this program in this timeslot every day.');
-define ('_LANG_RECTYPE_LONG_CHANNEL',       'Always record this program on channel.');
+define ('_LANG_RECTYPE_LONG_CHANNEL',       'Always record this program on channel ');
 define ('_LANG_RECTYPE_LONG_ALWAYS',        'Always record this program on any channel.');
 define ('_LANG_RECTYPE_LONG_WEEKLY',        'Record this program in this timeslot every week.');
 define ('_LANG_RECTYPE_LONG_FINDONE',       'Record one showing of this program at any time.');
