37ff28e2b03ea0a6af6995a9a1858abc45d82de7
Chris Petersen <cpetersen@mythtv.org>
2004-03-02 03:30:49 +0000

    print utf-8 content type header
    git-svn-id: http://svn.mythtv.org/svn/trunk@3327 7dbf422c-18fa-0310-86e9-fd20926502f2

--- status.php
+++ status.php
@@ -1,6 +1,6 @@
 <?php
 /***                                                                        ***\
-	status.php                      Last Updated: 2004.01.27 (xris)
+	status.php                               Last Updated: 2004.03.01 (xris)
 
 	This file is part of MythWeb, a php-based interface for MythTV.
 	See README and LICENSE for details.
@@ -12,6 +12,9 @@
 // Get the address/port of the master machine
 	$masterhost = get_backend_setting('MasterServerIP');
 	$statusport = get_backend_setting('BackendStatusPort');
+
+// Make sure the content is interpreted as UTF-8
+	header('Content-Type:  text/html; charset=UTF-8');
 
 // Print out the mythtv status page
 	readfile("http://$masterhost:$statusport");
