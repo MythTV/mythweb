dda6368ae0d655f692c5e820e5c7585318844e44
Chris Petersen <cpetersen@mythtv.org>
2005-02-07 00:37:11 +0000

    status now displays inline with the rest of mythweb!
    git-svn-id: http://svn.mythtv.org/svn/trunk@5444 7dbf422c-18fa-0310-86e9-fd20926502f2

--- status.php
+++ status.php
@@ -1,6 +1,6 @@
 <?php
 /***                                                                        ***\
-    status.php                               Last Updated: 2004.03.01 (xris)
+    status.php                               Last Updated: 2005.02.06 (xris)
 
     This file is part of MythWeb, a php-based interface for MythTV.
     See README and LICENSE for details.
@@ -16,8 +16,32 @@
 // Make sure the content is interpreted as UTF-8
     header('Content-Type:  text/html; charset=UTF-8');
 
-// Print out the mythtv status page
-    readfile("http://$masterhost:$statusport");
+// Load the status page
+    if (function_exists('file_get_contents'))
+        $page .= file_get_contents("http://$masterhost:$statusport");
+    else
+        $page .= implode("\n", file("http://$masterhost:$statusport"));
+
+// Extract the page title
+    preg_match('#<title>(.+?)</title>#s', $page, $title);
+    $title = $title[0];
+
+// Clean up the page, and add some invisible content with the actual URL grabbed
+    $page = "<!-- Obtained from:  http://$masterhost:$statusport -->\n"
+           .preg_replace('#\s*</body>\s*</html>\s*$#s', '',
+                preg_replace('/^.+?<body>\s*\n/s', "\n",
+                    $page
+                )
+            );
+
+// Load the class for this page
+    require_once theme_dir.'status.php';
+
+// Create the page object
+    $Page = new Theme_status;
+
+// Print the page
+    $Page->print_page($page, $title);
 
 ?>
 
