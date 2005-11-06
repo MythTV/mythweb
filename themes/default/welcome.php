a2e08f25e07975b5e029f940c214459228e47981
Chris Petersen <cpetersen@mythtv.org>
2005-11-06 06:32:46 +0000

    create a new lower case default theme directory so we can keep the old and new display code separate.
    git-svn-id: http://svn.mythtv.org/svn/trunk@7769 7dbf422c-18fa-0310-86e9-fd20926502f2

---
+++ themes/default/welcome.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * The display code for the main welcome page that lists the available mythweb
+ * sections.
+ *
+ * @url         $URL$
+ * @date        $Date$
+ * @version     $Revision$
+ * @author      $Author$
+ * @license     GPL
+ *
+ * @package     MythWeb
+ *
+/**/
+
+?>
