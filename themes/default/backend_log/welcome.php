1d1d752d106ef45cc51ae0488147eb1f107b482f
Chris Petersen <cpetersen@mythtv.org>
2005-12-06 04:10:28 +0000

    second chunk of a troublesome commit to move backend log display to the new format
    git-svn-id: http://svn.mythtv.org/svn/trunk@8132 7dbf422c-18fa-0310-86e9-fd20926502f2

---
+++ themes/default/backend_log/welcome.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * Welcome page description of the status module.
+ *
+ * @url         $URL$
+ * @date        $Date$
+ * @version     $Revision$
+ * @author      $Author$
+ * @license     GPL
+ *
+ * @package     MythWeb
+ *
+/**/
+
+// Open with a div and an image
+    echo '<div id="info_backend_log" class="hidden">',
+         '<img src="', skin_url, '/img/backend_log.png" class="module_icon" />',
+
+// Print a basic overview of what this module does
+         t('Show the server logs.'),
+
+// Close the div
+         "</div>\n";
