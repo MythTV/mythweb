6adac2255109e8a3ad5ded827883bdbddef7a7f8
Chris Petersen <cpetersen@mythtv.org>
2005-12-22 05:13:45 +0000

    patches from kormoc (backported from siliconmechanics.com code, with gpl license granted) to disable/reenable fatal errors in db.php.  Use this new code to make non-fatal tests for music and video meta data tables
    git-svn-id: http://svn.mythtv.org/svn/trunk@8344 7dbf422c-18fa-0310-86e9-fd20926502f2

--- modules/music/init.php
+++ modules/music/init.php
@@ -14,7 +14,10 @@
 /**/
 
 // First, we should check to see that MythMusic is configured.
+// Make sure to turn off fatal errors in case the table isn't there.
+    $db->disable_fatal_errors();
     $has_music = $db->query_col('SELECT COUNT(*) FROM musicmetadata');
+    $db->enable_fatal_errors();
 
 // If music is enabled, add it to the list.
     if ($has_music)
--- modules/video/init.php
+++ modules/video/init.php
@@ -14,7 +14,10 @@
 /**/
 
 // First, we should check to see that MythVideo is configured.
+// Make sure to turn off fatal errors in case the table isn't there.
+    $db->disable_fatal_errors();
     $has_video = $db->query_col('SELECT COUNT(*) FROM videometadata');
+    $db->enable_fatal_errors();
 
 // If video is enabled, add it to the list.
     if ($has_video)
