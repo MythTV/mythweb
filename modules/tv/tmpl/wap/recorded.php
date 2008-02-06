798bda65d684cd15ded446d40fac11396d503ae6
Rob Smith <rsmith@mythtv.org>
2008-02-06 03:53:09 +0000

    Fixes #3721, this updates the url calls to correctly delete the files in WAP mode. Thanks isismanor.
    git-svn-id: http://svn.mythtv.org/svn/trunk@15799 7dbf422c-18fa-0310-86e9-fd20926502f2

--- modules/tv/tmpl/wap/recorded.php
+++ modules/tv/tmpl/wap/recorded.php
@@ -55,8 +55,8 @@
         if ($show->endtime > time()) { ?>
             <font color="#FF0000">currently recording - </font>
 <?php   } ?>
-            <b></b><a id="delete_<?php echo $row ?>" href="<?php echo root ?>tv/recorded?delete=yes&file=<?php echo urlencode($show->filename) ?>">Delete </a></b>
-            <b></b><a id="delete_<?php echo $row ?>" href="<?php echo root ?>tv/recorded?delete=yes&file=<?php echo urlencode($show->filename) ?>&forget_old">Rerecord</a></b><br />
+            <b></b><a id="delete_<?php echo $row ?>" href="<?php echo root ?>tv/recorded?delete=yes&chanid=<?php echo $show->chanid ?>&starttime=<?php echo $show->recstartts ?>">Delete </a></b> 
+                <b></b><a id="delete_<?php echo $row ?>" href="<?php echo root ?>tv/recorded?delete=yes&chanid=<?php echo $show->chanid ?>&starttime=<?php echo $show->recstartts ?>&forget_old">Rerecord</a></b><br /> 
 <?php
         echo "<br />";
 
