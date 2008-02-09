aad124f7b42ef095e73724de023afcbb9ef83dd1
Rob Smith <rsmith@mythtv.org>
2008-02-09 11:00:39 +0000

    Browser.js does not appear used anymore, deleting
    git-svn-id: http://svn.mythtv.org/svn/trunk@15868 7dbf422c-18fa-0310-86e9-fd20926502f2

--- modules/_shared/tmpl/default/header.php
+++ modules/_shared/tmpl/default/header.php
@@ -35,7 +35,6 @@
     <script type="text/javascript" src="<?php echo root ?>js/prototip/prototip.js"></script>
     <link rel="stylesheet" type="text/css" href="<?php echo root; ?>js/prototip/prototip.css" />
 
-    <script type="text/javascript" src="<?php echo root ?>js/browser.js"></script>
     <script type="text/javascript" src="<?php echo root ?>js/utils.js"></script>
     <script type="text/javascript" src="<?php echo root ?>js/ajax_utils.js"></script>
     <script type="text/javascript" src="<?php echo root ?>js/AC_OETags.js"></script>
