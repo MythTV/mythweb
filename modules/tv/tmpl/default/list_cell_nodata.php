cad619409a4d1d8d107a20f6dc583307b0c6f379
Chris Petersen <cpetersen@mythtv.org>
2008-02-16 23:47:47 +0000

    removing this to prep for another resync with trunk
    git-svn-id: http://svn.mythtv.org/svn/branches/release-0-20-fixes@16077 7dbf422c-18fa-0310-86e9-fd20926502f2

--- modules/tv/tmpl/default/list_cell_nodata.php
+++
@@ -1,3 +0,0 @@
-<td class="small tv_Unknown" colspan="<?php echo $timeslots_used ?>" valign="top"><?php echo t('NO DATA') ?></td>
-
-
--- modules/tv/tmpl/wml/list_cell_program.php
+++
@@ -1,7 +0,0 @@
-<?php
-    echo $program->channel->callsign.'  '; //chanid." ";
-    echo strftime($_SESSION['time_format'], $program->starttime);
-    echo ' - <a href="'.root.'tv/detail/'.$program->chanid.'/'.$program->starttime.'">';
-    echo htmlspecialchars($program->title);
-    echo "</a><br />\n";
-?>
