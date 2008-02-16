MythTV/mythweb

commit 404bd55f5592eb84eb80fc514ac22377b26b0766

Switch over the only function that uses the old ajax javascript functions, so remove them, also add in JS to PHP conversion lib
git-svn-id: http://svn.mythtv.org/svn/trunk@15862 7dbf422c-18fa-0310-86e9-fd20926502f2

modules/tv/tmpl/default/recorded.php
@@ -112,28 +112,36 @@
     function confirm_delete(id, forget_old) {
         var file = files[id];
         if (confirm("<?php echo t('Are you sure you want to delete the following show?')
-                    ?>\n\n     "+file.title + ((file.subtitle == '') ? "" : ": " +file.subtitle))) { 
-            var url = '<?php echo root ?>tv/recorded?delete=yes&chanid='+file.chanid
-                      +'&starttime='+file.starttime
-                      +(forget_old
-                        ? '&forget_old=yes'
-                        : '');
+                    ?>\n\n     "+file.title + ((file.subtitle == '') ? "" : ": " +file.subtitle))) {
         // Do the actual deletion
             if (programs_shown == 1)
                 location.href = url;
             else {
                 ajax_add_request();
-                submit_url(url+'&ajax=yes', http_success, http_failure, id, file);
+                new Ajax.Request('<?php echo root; ?>tv/recorded',
+                                 {
+                                    method: 'post',
+                                    onSuccess: http_success,
+                                    onFailue: http_failure,
+                                    parameters: { ajax:       'yes',
+                                                  delete:     'yes',
+                                                  chanid:     file.chanid,
+                                                  starttime:  file.starttime,
+                                                  forget_old: (forget_old ? 'yes' : 'no'),
+                                                  id:         id,
+                                                  file:       JSON.stringify(file)
+                                                }
+                                });
             }
-        // Debug statements - uncomment to verify that the right file is being deleted
-            //alert('row number ' + id + ' belonged to section ' + section + ' which now has ' + rowcount[section] + ' elements');
-            //alert('just deleted an episode of "' + title + '" which now has ' + episode_count + ' episodes left');
+        // Debug statements - uncomment to verify that the right file is being deleted. Now with firebug goodness
+            //console.log('row number ' + id + ' belonged to section ' + section + ' which now has ' + rowcount[section] + ' elements');
+            //console.log('just deleted an episode of "' + title + '" which now has ' + episode_count + ' episodes left');
         }
     }
 
-    function http_success(result, args) {
-        var id   = args.shift();
-        var file = args.shift();
+    function http_success(result) {
+        var id   = result.responseJSON['id'];
+        var file = JSON.parse(result.responseJSON['file']);
     // Hide the row from view
         $('inforow_' + id).toggle();
         $('statusrow_' + id).toggle();
@@ -190,14 +198,14 @@
         diskused -= file.size;
         $('diskused').innerHTML = nice_filesize(diskused);
     // Adjust the freespace shown
-        $('diskfree').innerHTML = nice_filesize(<?php echo disk_size ?> - diskused);
+        $('diskfree').innerHTML = nice_filesize(<?php echo disk_size; ?> - diskused);
         // Eventually, we should perform the removal-from-the-list here instead
         // of in confirm_delete()
         ajax_remove_request();
     }
 
-    function http_failure(err, errstr, args) {
-        var file = args[0];
+    function http_failure(err, errstr) {
+        var file = JSON.parse(result.responseJSON['file']);
         alert("Can't delete "+file.title+': '+file.subtitle+".\nHTTP Error:  " + errstr + ' (' + err + ')');
         ajax_remove_request();
     }
@@ -425,5 +433,3 @@ EOM;
 
 // Print the page footer
     require 'modules/_shared/tmpl/'.tmpl.'/footer.php';
-
-

modules/tv/recorded.php
@@ -20,13 +20,10 @@
     require_once 'includes/sorting.php';
 
 // Delete or undelete a program?
-    isset($_GET['forget_old']) or $_GET['forget_old'] = $_POST['forget_old'];
-    isset($_GET['delete'])     or $_GET['delete']     = $_POST['delete'];
-    isset($_GET['file'])       or $_GET['file']       = $_POST['file'];
-    if ($_GET['delete'] || $_GET['undelete']) {
-        if ($_GET['delete'])
+    if ($_REQUEST['delete'] || $_REQUEST['undelete']) {
+        if ($_REQUEST['delete'])
           $backendstr = 'DELETE_RECORDING';
-        else 
+        else
           $backendstr = 'UNDELETE_RECORDING';
     // Keep a previous-row counter to return to after deleting
         $prev_row = -2;
@@ -36,19 +33,22 @@
             if (($_SESSION['recorded_title'] == $row[0]) || ($_SESSION['recorded_title'] == ''))
                 $prev_row++;
         // This row isn't the one we're looking for
-            if ($row[4] != $_GET['chanid'] || $row[26] != $_GET['starttime'])
+            if ($row[4] != $_REQUEST['chanid'] || $row[26] != $_REQUEST['starttime'])
                 continue;
         // Delete the recording
             backend_command(array($backendstr, implode(backend_sep, $row), '0'));
         // Forget all knowledge of old recordings?
-            if (isset($_GET['forget_old'])) {
+            if (isset($_REQUEST['forget_old'])) {
                 backend_command(array('FORGET_RECORDING', implode(backend_sep, $row), '0'));
             // Delay a second so the scheduler can catch up
                 sleep(1);
             }
         // Exit early if we're in AJAX mode.
-            if (isset($_GET['ajax'])) {
-                echo 'success';
+            if (isset($_REQUEST['ajax'])) {
+                header('Content-Type: application/json');
+                $JSON = new Services_JSON();
+                echo $JSON->encode(array('id'   => $_REQUEST['id'],
+                                         'file' => $_REQUEST['file']));
                 exit;
             }
         // No need to scan the rest of the items, so leave early
@@ -70,10 +70,8 @@
     }
 
 // Queries for a specific program title
-    isset($_GET['title'])    or $_GET['title']    = $_POST['title'];
-    isset($_GET['recgroup']) or $_GET['recgroup'] = $_POST['recgroup'];
-    isset($_GET['title'])    or $_GET['title']    = isset($_GET['refresh']) ? '' : $_SESSION['recorded_title'];
-    isset($_GET['recgroup']) or $_GET['recgroup'] = isset($_GET['refresh']) ? '' : $_SESSION['recorded_recgroup'];
+    isset($_REQUEST['title'])    or $_REQUEST['title']    = isset($_REQUEST['refresh']) ? '' : $_SESSION['recorded_title'];
+    isset($_REQUEST['recgroup']) or $_REQUEST['recgroup'] = isset($_REQUEST['refresh']) ? '' : $_SESSION['recorded_recgroup'];
 
 // Parse the program list
     $warning    = NULL;
@@ -110,18 +108,18 @@
             $Total_Programs++;
             $Groups[$record[30]]++;
         // Hide LiveTV  and Deleted recordings from the title list
-            if (($_GET['recgroup'] && $_GET['recgroup'] == $record[30]) || (!$_GET['recgroup'] && $record[30] != 'LiveTV' && $record[30] != 'Deleted'))
+            if (($_REQUEST['recgroup'] && $_REQUEST['recgroup'] == $record[30]) || (!$_REQUEST['recgroup'] && $record[30] != 'LiveTV' && $record[30] != 'Deleted'))
                 $Program_Titles[$record[0]]++;
         // Skip files with no chanid
             if (!$record[4])
                 continue;
         // Skip programs the user doesn't want to look at
-            if ($_GET['title'] && $_GET['title'] != $record[0])
+            if ($_REQUEST['title'] && $_REQUEST['title'] != $record[0])
                 continue;
-            if ($_GET['recgroup'] && $_GET['recgroup'] != $record[30])
+            if ($_REQUEST['recgroup'] && $_REQUEST['recgroup'] != $record[30])
                 continue;
         // Hide LiveTV recordings from the default view
-            if (empty($_GET['recgroup']) && ($record[30] == 'LiveTV' || $record[30] == 'Deleted'))
+            if (empty($_REQUEST['recgroup']) && ($record[30] == 'LiveTV' || $record[30] == 'Deleted'))
                 continue;
         // Make sure that everything we're dealing with is an array
             if (!is_array($Programs[$record[0]]))
@@ -130,25 +128,25 @@
             $Programs[$record[0]][] = $record;
         }
     // Did we try to view a program that we don't have recorded?  Revert to showing all programs
-        if ($Total_Programs > 0 && !count($Programs) && !isset($_GET['refresh'])) {
+        if ($Total_Programs > 0 && !count($Programs) && !isset($_REQUEST['refresh'])) {
         // Requested the "All" mode, but there are no recordings
-            if (empty($_GET['title']) && empty($_GET['recgroup'])) {
+            if (empty($_REQUEST['title']) && empty($_REQUEST['recgroup'])) {
                 if ($Groups['LiveTV'] > 0) {
                     $warning = t('Showing all programs from the $1 group.', 'LiveTV');
-                    $_GET['recgroup'] = 'LiveTV';
+                    $_REQUEST['recgroup'] = 'LiveTV';
                     continue;
                 }
             }
         // Requested a title that's not in the requested group
-            if ($_GET['recgroup'] && $_GET['title'] && $Groups[$_GET['recgroup']] > 0) {
-                $warning = t('Showing all programs from the $1 group.', $_GET['recgroup']);
-                unset($_GET['title']);
+            if ($_REQUEST['recgroup'] && $_REQUEST['title'] && $Groups[$_REQUEST['recgroup']] > 0) {
+                $warning = t('Showing all programs from the $1 group.', $_REQUEST['recgroup']);
+                unset($_REQUEST['title']);
                 continue;
             }
         // Catch anything else
-            $_GET['refresh'] = true;
+            $_REQUEST['refresh'] = true;
             $warning         = t('Showing all programs.');
-            unset($_GET['title'], $_GET['recgroup']);
+            unset($_REQUEST['title'], $_REQUEST['recgroup']);
             continue;
         }
     // Did the best we could to find some programs; let's move on.
@@ -178,8 +176,8 @@
     ksort($Groups);
 
 // Keep track of the program/title the user wants to view
-    $_SESSION['recorded_title']    = $_GET['title'];
-    $_SESSION['recorded_recgroup'] = $_GET['recgroup'];
+    $_SESSION['recorded_title']    = $_REQUEST['title'];
+    $_SESSION['recorded_recgroup'] = $_REQUEST['recgroup'];
 
 // The default sorting choice isn't so good for recorded programs, so we'll set our own default
     if (!is_array($_SESSION['recorded_sortby']) || !count($_SESSION['recorded_sortby']))
@@ -222,4 +220,3 @@
 
 // Exit
     exit;
-

js/ajax.js
@@ -1,116 +1,0 @@
-/**
- * A small but growing library of generic AJAX routines.
- *  - Sync and async ajax use.
- *  - Plaintext responses [non-XML]
- *  - Browser Support: Safari, Opera, Firefox
- *
- * @url         $URL$
- * @date        $Date$
- * @version     $Revision$
- * @author      $Author$
- * @license     LGPL
- *
- * @package     MythWeb
- * @subpackage  Javascript
- *
-/**/
-
-// Global shared variable
-    var shared_httpobj         = new Array();
-    var shared_success_handler = new Array();
-    var shared_failure_handler = new Array();
-    var shared_handler_args    = new Array();
-
-// Initialize an http connection object
-    function get_connection_object() {
-        var httpobj_temp;
-        try {
-            httpobj_temp = new ActiveXObject('Msxml2.XMLHTTP');
-        }
-        catch (e) {
-            try {
-                httpobj_temp = new ActiveXObject('Microsoft.XMLHTTP');
-            } catch (oc) {
-                httpobj_temp = null;
-            }
-        }
-        if (!httpobj_temp && typeof XMLHttpRequest != 'undefined')
-            httpobj_temp = new XMLHttpRequest();
-        return httpobj_temp;
-    }
-
-// Submit a simple URL and execute optional success_handler if it succeeds (or
-// optional failure_handler if it fails).  handler_args are any additional
-// arguments passed in, and they are passed as an array to the success/failure
-// handler functions.
-    function submit_url( url, success_handler , failure_handler /* [handler_args, ...] */ ) {
-    // Optional Parameters won't be introduced until Javascript 2.0
-        success_handler = (success_handler ? success_handler : null);
-        failure_handler = (failure_handler ? failure_handler : null);
-    // Create a new connection object
-        var httpobj = get_connection_object();
-    // Add it to the array, and get the length
-        var index = shared_httpobj.push(httpobj);
-    // figure out the actual item index, which is one less then the length (arrays start at 0)
-        index--;
-        shared_handler_args[index] = new Array();
-        for (var i=3; i<arguments.length; i++) {
-            shared_handler_args[index].push(arguments[i]);
-        }
-        shared_success_handler[index] = success_handler;
-        shared_failure_handler[index] = failure_handler;
-    // If we have no success handler and no failure handler, we assume that we want to do a blocking call rather then async.
-        if (success_handler == null && failure_handler == null ) {
-        // Set up the query
-            httpobj.open('GET', url, false);
-        // Prevent the browser cache from being used [for documents post-1994]
-        // Safari Bug: Safari will mark the satus as 'undefined' instead of 200 if a page is cached [bad]
-            httpobj.setRequestHeader('If-Modified-Since','Wed, 15 Nov 1995 00:00:00 GMT');
-        // Submit the query
-            httpobj.send(null);
-        // Return the data
-            return (httpobj.responseText);
-        }
-        else {
-        // Set up the query
-            httpobj.open('GET', url, true);
-        // Set up a response handler
-            httpobj.onreadystatechange = ajax_check;
-        // Submit the query
-            httpobj.send(null);
-        }
-    }
-
-    function ajax_check() {
-        for (var i=0; i<shared_httpobj.length; i++) {
-            httpobj = shared_httpobj[i];
-            if (httpobj.readyState == 4) {
-                handler_args    = shared_handler_args[i];
-                success_handler = shared_success_handler[i];
-                failure_handler = shared_failure_handler[i];
-            // Remove the used up object from the array;
-                shared_httpobj.splice(i,1);
-                shared_success_handler.splice(i,1);
-                shared_failure_handler.splice(i,1);
-                shared_handler_args.splice(i,1);
-            // Success
-                if (httpobj.status == '200') {
-                // Custom success handler receives the returned content as a parameter.
-                // There is no default success handler
-                    if (success_handler != null) {
-                        success_handler(httpobj.responseText, handler_args);
-                    }
-                }
-            // Failure
-                else {
-                // Default failure handler
-                    if (failure_handler == null)
-                        alert('HTTP Error:  ' + httpobj.statusText + ' (' + httpobj.status + ')');
-                // Custom failure handler receives status and statusText as parameters.
-                    else
-                        failure_handler(httpobj.status, httpobj.statusText, handler_args);
-                }
-
-            }
-        }
-    }

js/json2.js
@@ -1,0 +1,273 @@
+/*
+    json2.js
+    2008-01-17
+
+    Public Domain
+
+    No warranty expressed or implied. Use at your own risk.
+
+    See http://www.JSON.org/js.html
+
+    This file creates a global JSON object containing two methods:
+
+        JSON.stringify(value, whitelist)
+            value       any JavaScript value, usually an object or array.
+
+            whitelist   an optional array parameter that determines how object
+                        values are stringified.
+
+            This method produces a JSON text from a JavaScript value.
+            There are three possible ways to stringify an object, depending
+            on the optional whitelist parameter.
+
+            If an object has a toJSON method, then the toJSON() method will be
+            called. The value returned from the toJSON method will be
+            stringified.
+
+            Otherwise, if the optional whitelist parameter is an array, then
+            the elements of the array will be used to select members of the
+            object for stringification.
+
+            Otherwise, if there is no whitelist parameter, then all of the
+            members of the object will be stringified.
+
+            Values that do not have JSON representaions, such as undefined or
+            functions, will not be serialized. Such values in objects will be
+            dropped; in arrays will be replaced with null.
+            JSON.stringify(undefined) returns undefined. Dates will be
+            stringified as quoted ISO dates.
+
+            Example:
+
+            var text = JSON.stringify(['e', {pluribus: 'unum'}]);
+            // text is '["e",{"pluribus":"unum"}]'
+
+        JSON.parse(text, filter)
+            This method parses a JSON text to produce an object or
+            array. It can throw a SyntaxError exception.
+
+            The optional filter parameter is a function that can filter and
+            transform the results. It receives each of the keys and values, and
+            its return value is used instead of the original value. If it
+            returns what it received, then structure is not modified. If it
+            returns undefined then the member is deleted.
+
+            Example:
+
+            // Parse the text. If a key contains the string 'date' then
+            // convert the value to a date.
+
+            myData = JSON.parse(text, function (key, value) {
+                return key.indexOf('date') >= 0 ? new Date(value) : value;
+            });
+
+    This is a reference implementation. You are free to copy, modify, or
+    redistribute.
+
+    Use your own copy. It is extremely unwise to load third party
+    code into your pages.
+*/
+
+/*jslint evil: true */
+
+/*global JSON */
+
+/*members "\b", "\t", "\n", "\f", "\r", "\"", JSON, "\\", apply,
+    charCodeAt, floor, getUTCDate, getUTCFullYear, getUTCHours,
+    getUTCMinutes, getUTCMonth, getUTCSeconds, hasOwnProperty, join, length,
+    parse, propertyIsEnumerable, prototype, push, replace, stringify, test,
+    toJSON, toString
+*/
+
+if (!this.JSON) {
+
+    JSON = function () {
+
+        function f(n) {    // Format integers to have at least two digits.
+            return n < 10 ? '0' + n : n;
+        }
+
+        Date.prototype.toJSON = function () {
+
+// Eventually, this method will be based on the date.toISOString method.
+
+            return this.getUTCFullYear()   + '-' +
+                 f(this.getUTCMonth() + 1) + '-' +
+                 f(this.getUTCDate())      + 'T' +
+                 f(this.getUTCHours())     + ':' +
+                 f(this.getUTCMinutes())   + ':' +
+                 f(this.getUTCSeconds())   + 'Z';
+        };
+
+
+        var m = {    // table of character substitutions
+            '\b': '\\b',
+            '\t': '\\t',
+            '\n': '\\n',
+            '\f': '\\f',
+            '\r': '\\r',
+            '"' : '\\"',
+            '\\': '\\\\'
+        };
+
+        function stringify(value, whitelist) {
+            var a,          // The array holding the partial texts.
+                i,          // The loop counter.
+                k,          // The member key.
+                l,          // Length.
+                r = /["\\\x00-\x1f\x7f-\x9f]/g,
+                v;          // The member value.
+
+            switch (typeof value) {
+            case 'string':
+
+// If the string contains no control characters, no quote characters, and no
+// backslash characters, then we can safely slap some quotes around it.
+// Otherwise we must also replace the offending characters with safe sequences.
+
+                return r.test(value) ?
+                    '"' + value.replace(r, function (a) {
+                        var c = m[a];
+                        if (c) {
+                            return c;
+                        }
+                        c = a.charCodeAt();
+                        return '\\u00' + Math.floor(c / 16).toString(16) +
+                                                   (c % 16).toString(16);
+                    }) + '"' :
+                    '"' + value + '"';
+
+            case 'number':
+
+// JSON numbers must be finite. Encode non-finite numbers as null.
+
+                return isFinite(value) ? String(value) : 'null';
+
+            case 'boolean':
+            case 'null':
+                return String(value);
+
+            case 'object':
+
+// Due to a specification blunder in ECMAScript,
+// typeof null is 'object', so watch out for that case.
+
+                if (!value) {
+                    return 'null';
+                }
+
+// If the object has a toJSON method, call it, and stringify the result.
+
+                if (typeof value.toJSON === 'function') {
+                    return stringify(value.toJSON());
+                }
+                a = [];
+                if (typeof value.length === 'number' &&
+                        !(value.propertyIsEnumerable('length'))) {
+
+// The object is an array. Stringify every element. Use null as a placeholder
+// for non-JSON values.
+
+                    l = value.length;
+                    for (i = 0; i < l; i += 1) {
+                        a.push(stringify(value[i], whitelist) || 'null');
+                    }
+
+// Join all of the elements together and wrap them in brackets.
+
+                    return '[' + a.join(',') + ']';
+                }
+                if (whitelist) {
+
+// If a whitelist (array of keys) is provided, use it to select the components
+// of the object.
+
+                    l = whitelist.length;
+                    for (i = 0; i < l; i += 1) {
+                        k = whitelist[i];
+                        if (typeof k === 'string') {
+                            v = stringify(value[k], whitelist);
+                            if (v) {
+                                a.push(stringify(k) + ':' + v);
+                            }
+                        }
+                    }
+                } else {
+
+// Otherwise, iterate through all of the keys in the object.
+
+                    for (k in value) {
+                        if (typeof k === 'string') {
+                            v = stringify(value[k], whitelist);
+                            if (v) {
+                                a.push(stringify(k) + ':' + v);
+                            }
+                        }
+                    }
+                }
+
+// Join all of the member texts together and wrap them in braces.
+
+                return '{' + a.join(',') + '}';
+            }
+        }
+
+        return {
+            stringify: stringify,
+            parse: function (text, filter) {
+                var j;
+
+                function walk(k, v) {
+                    var i, n;
+                    if (v && typeof v === 'object') {
+                        for (i in v) {
+                            if (Object.prototype.hasOwnProperty.apply(v, [i])) {
+                                n = walk(i, v[i]);
+                                if (n !== undefined) {
+                                    v[i] = n;
+                                }
+                            }
+                        }
+                    }
+                    return filter(k, v);
+                }
+
+
+// Parsing happens in three stages. In the first stage, we run the text against
+// regular expressions that look for non-JSON patterns. We are especially
+// concerned with '()' and 'new' because they can cause invocation, and '='
+// because it can cause mutation. But just to be safe, we want to reject all
+// unexpected forms.
+
+// We split the first stage into 4 regexp operations in order to work around
+// crippling inefficiencies in IE's and Safari's regexp engines. First we
+// replace all backslash pairs with '@' (a non-JSON character). Second, we
+// replace all simple value tokens with ']' characters. Third, we delete all
+// open brackets that follow a colon or comma or that begin the text. Finally,
+// we look to see that the remaining characters are only whitespace or ']' or
+// ',' or ':' or '{' or '}'. If that is so, then the text is safe for eval.
+
+                if (/^[\],:{}\s]*$/.test(text.replace(/\\./g, '@').
+replace(/"[^"\\\n\r]*"|true|false|null|-?\d+(?:\.\d*)?(?:[eE][+\-]?\d+)?/g, ']').
+replace(/(?:^|:|,)(?:\s*\[)+/g, ''))) {
+
+// In the second stage we use the eval function to compile the text into a
+// JavaScript structure. The '{' operator is subject to a syntactic ambiguity
+// in JavaScript: it can begin a block or an object literal. We wrap the text
+// in parens to eliminate the ambiguity.
+
+                    j = eval('(' + text + ')');
+
+// In the optional third stage, we recursively walk the new structure, passing
+// each name/value pair to a filter function for possible transformation.
+
+                    return typeof filter === 'function' ? walk('', j) : j;
+                }
+
+// If the text is not JSON parseable, then a SyntaxError is thrown.
+
+                throw new SyntaxError('parseJSON');
+            }
+        };
+    }();
+}

modules/_shared/tmpl/default/header.php
@@ -32,14 +32,15 @@
     <meta http-equiv="content-type" content="text/html; charset=utf-8" />
 
     <script type="text/javascript" src="<?php echo root ?>js/prototype.js"></script>
+    <script type="text/javascript" src="<?php echo root ?>js/json2.js"></script>
+    <script type="text/javascript" src="<?php echo root ?>js/prototip/prototip.js"></script>
+    <link rel="stylesheet" type="text/css" href="<?php echo root; ?>js/prototip/prototip.css" />
+
     <script type="text/javascript" src="<?php echo root ?>js/browser.js"></script>
     <script type="text/javascript" src="<?php echo root ?>js/utils.js"></script>
     <script type="text/javascript" src="<?php echo root ?>js/mouseovers.js"></script>
-    <script type="text/javascript" src="<?php echo root ?>js/ajax.js"></script>
     <script type="text/javascript" src="<?php echo root ?>js/ajax_utils.js"></script>
     <script type="text/javascript" src="<?php echo root ?>js/AC_OETags.js"></script>
-    <script type="text/javascript" src="<?php echo root ?>js/prototip/prototip.js"></script>
-    <link rel="stylesheet" type="text/css" href="<?php echo root; ?>js/prototip/prototip.css" />
 
     <script language="JavaScript" type="text/javascript">
         <!--

commit 0186858a555bfe10a87926a5a07f8d2c972a0257

restore a redirect that was accidentally broken in r15862
git-svn-id: http://svn.mythtv.org/svn/trunk@16070 7dbf422c-18fa-0310-86e9-fd20926502f2

modules/tv/tmpl/default/recorded.php
@@ -115,7 +115,12 @@
                     ?>\n\n     "+file.title + ((file.subtitle == '') ? "" : ": " +file.subtitle))) {
         // Do the actual deletion
             if (programs_shown == 1)
-                location.href = url;
+                location.href = '<?php echo root ?>tv/recorded?delete=yes&chanid='+file.chanid
+                                +'&starttime='+file.starttime
+                                +(forget_old
+                                    ? '&forget_old=yes'
+                                    : ''
+                                 );
             else {
                 ajax_add_request();
                 new Ajax.Request('<?php echo root; ?>tv/recorded',