a674817861f2b735df882f7481ce1236ec1b165f
Rob Smith <rsmith@mythtv.org>
2008-12-08 21:18:59 +0000

    Update the class autoloader to use _ as path seperators, remove the include error surpression
    git-svn-id: http://svn.mythtv.org/svn/trunk@19292 7dbf422c-18fa-0310-86e9-fd20926502f2

--- includes/class_autoload.php
+++ includes/class_autoload.php
@@ -17,12 +17,15 @@
 
     function __autoload($className) {
         global $Path;
+        $className = str_replace('_', '/', $className);
         if (file_exists("classes/$className.php"))
-            @include_once "classes/$className.php";
+            include_once "classes/$className.php";
         elseif (file_exists(modules_path.'/'.module."/classes/$className.php"))
-            @include_once modules_path.'/'.module."/classes/$className.php";
+            include_once modules_path.'/'.module."/classes/$className.php";
         elseif (file_exists(modules_path.'/'.$Path[1]."/classes/$className.php"))
-            @include_once modules_path.'/'.$Path[1]."/classes/$className.php";
+            include_once modules_path.'/'.$Path[1]."/classes/$className.php";
         elseif (file_exists(modules_path.'/'.$Path[0]."/classes/$className.php"))
-            @include_once modules_path.'/'.$Path[0]."/classes/$className.php";
+            include_once modules_path.'/'.$Path[0]."/classes/$className.php";
+        else
+            trigger_error(t("Failure to autoload class $className!"), E_USER_ERROR);
     }
