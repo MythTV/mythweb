67e67bab8ee42029945f2888ede589ed554e4d85
Chris Petersen <cpetersen@mythtv.org>
2009-05-09 05:50:06 +0000

    more path issues in iPod templates
    git-svn-id: http://svn.mythtv.org/svn/trunk@20519 7dbf422c-18fa-0310-86e9-fd20926502f2

--- modules/_shared/tmpl/iPod/footer.php
+++ modules/_shared/tmpl/iPod/footer.php
@@ -19,13 +19,13 @@
     // it refuses to load other images
     // so background-images changes fail
         var Image1 = new Image();
-        Image1.src = '<?php echo skin_url; ?>/img/chevron_touched.png';
+        Image1.src = '<?php echo skin_url ?>img/chevron_touched.png';
         var Image2 = new Image();
-        Image2.src = '<?php echo skin_url; ?>/img/item_background_touched.png';
+        Image2.src = '<?php echo skin_url ?>img/item_background_touched.png';
         var Image3 = new Image();
-        Image3.src = '<?php echo skin_url; ?>/img/back_button_clicked.png';
+        Image3.src = '<?php echo skin_url ?>img/back_button_clicked.png';
         var Image4 = new Image();
-        Image4.src = '<?php echo skin_url; ?>/img/button_darkgray_pressed.png';
+        Image4.src = '<?php echo skin_url ?>img/button_darkgray_pressed.png';
     </script>
 
 </body>
