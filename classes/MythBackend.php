cd2663bbd13f5d18e7eed995229160667d339552
Chris Pinkham <cpinkham@mythtv.org>
2009-08-16 02:27:05 +0000

    Fix one user of BackendQueryDiskSpace that I missed in [21297] and bump the protocol version since the wire protocol did change with the addition of the block size to FileSystemInfo.
    Refs #6846.
    
    
    
    git-svn-id: http://svn.mythtv.org/svn/trunk@21298 7dbf422c-18fa-0310-86e9-fd20926502f2

--- classes/MythBackend.php
+++ classes/MythBackend.php
@@ -15,7 +15,7 @@ class MythBackend {
 
 // MYTH_PROTO_VERSION is defined in libmyth in mythtv/libs/libmyth/mythcontext.h
 // and should be the current MythTV protocol version.
-    static $protocol_version        = 46;
+    static $protocol_version        = 47;
 
 // The character string used by the backend to separate records
     static $backend_separator       = '[]:[]';
