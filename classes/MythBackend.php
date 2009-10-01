87a45aa9ff72bc54133fabf7e702e11093371dff
Daniel Kristjansson <dkristjansson@mythtv.org>
2009-10-01 19:43:38 +0000

    Refs #7195. Speed up Watch Recordings preview pixmap downloads from remote backends.
    Adds a new myth proto command, so all frontends and backends need to be updated.
    
    git-svn-id: http://svn.mythtv.org/svn/trunk@22164 7dbf422c-18fa-0310-86e9-fd20926502f2

--- classes/MythBackend.php
+++ classes/MythBackend.php
@@ -15,7 +15,7 @@ class MythBackend {
 
 // MYTH_PROTO_VERSION is defined in libmyth in mythtv/libs/libmyth/mythcontext.h
 // and should be the current MythTV protocol version.
-    static $protocol_version        = 48;
+    static $protocol_version        = 49;
 
 // The character string used by the backend to separate records
     static $backend_separator       = '[]:[]';
