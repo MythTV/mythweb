287fd3d60ec9ff487e432481f7f168ccc58c8bca
Mike Dean <mdean@mythtv.org>
2010-05-23 02:19:23 +0000

    Fix channel in MythWeb/PHP bindings.
    Apply changes equivalent to those in [24799] to the (currently-unused?) MythWeb/PHP bindings.
    
    
    git-svn-id: http://svn.mythtv.org/svn/trunk@24802 7dbf422c-18fa-0310-86e9-fd20926502f2

--- classes/MythTVChannel.php
+++ classes/MythTVChannel.php
@@ -5,18 +5,19 @@ class MythTVChannel {
 
     var $atsc_major_chan;
     var $atsc_minor_chan;
-    var $atscsrcid;
     var $brightness;
     var $callsign;
     var $chanid;
     var $channum;
     var $colour;
-    var $commfree;
+    var $commmethod;
     var $contrast;
+    var $default_authority;
     var $finetune;
     var $freqid;
     var $hue;
     var $icon;
+    var $last_record;
     var $mplexid;
     var $name;
     var $outputfilters;
@@ -32,6 +33,7 @@ class MythTVChannel {
 
     var $dtv_bandwidth;
     var $dtv_constellation;
+    var $dtv_default_authority;
     var $dtv_fec;
     var $dtv_frequency;
     var $dtv_guard_interval;
@@ -40,9 +42,11 @@ class MythTVChannel {
     var $dtv_inversion;
     var $dtv_lp_code_rate;
     var $dtv_modulation;
+    var $dtv_mod_sys;
     var $dtv_mplexid;
     var $dtv_networkid;
     var $dtv_polarity;
+    var $dtv_rolloff;
     var $dtv_serviceversion;
     var $dtv_sistandard;
     var $dtv_sourceid;
@@ -61,6 +65,7 @@ class MythTVChannel {
         $channel = $this->MythTV->DB->query_assoc('SELECT channel.*,
                                                           dtv_multiplex.bandwidth         AS dtv_bandwidth,
                                                           dtv_multiplex.constellation     AS dtv_constellation,
+                                                          dtv_multiplex.default_authority AS dtv_default_authority,
                                                           dtv_multiplex.fec               AS dtv_fec,
                                                           dtv_multiplex.frequency         AS dtv_frequency,
                                                           dtv_multiplex.guard_interval    AS dtv_guard_interval,
@@ -69,9 +74,11 @@ class MythTVChannel {
                                                           dtv_multiplex.inversion         AS dtv_inversion,
                                                           dtv_multiplex.lp_code_rate      AS dtv_lp_code_rate,
                                                           dtv_multiplex.modulation        AS dtv_modulation,
+                                                          dtv_multiplex.mod_sys           AS dtv_mod_sys,
                                                           dtv_multiplex.mplexid           AS dtv_mplexid,
                                                           dtv_multiplex.networkid         AS dtv_networkid,
                                                           dtv_multiplex.polarity          AS dtv_polarity,
+                                                          dtv_multiplex.rolloff           AS dtv_rolloff,
                                                           dtv_multiplex.serviceversion    AS dtv_serviceversion,
                                                           dtv_multiplex.sistandard        AS dtv_sistandard,
                                                           dtv_multiplex.sourceid          AS dtv_sourceid,
