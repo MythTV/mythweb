fe574dd9c74005671da48bca5ccc3e6031443535
Rob Smith <rsmith@mythtv.org>
2010-06-08 05:54:24 +0000

    Enhance the cache code to reduce overhead
    git-svn-id: http://svn.mythtv.org/svn/trunk@25043 7dbf422c-18fa-0310-86e9-fd20926502f2

--- classes/MythBase.php
+++ classes/MythBase.php
@@ -22,7 +22,12 @@ class MythBase {
         $class = get_called_class();
         $args = func_get_args();
         $key = self::getCacheKey($class, $args);
-        if (!isset(self::$instances[$key])) {
+        if (is_null($key)) {
+            $r = new ReflectionClass($class);
+            $obj = $r->newInstanceArgs($args);
+            return $obj;
+        }
+        elseif (!isset(self::$instances[$key])) {
             self::$instances[$key] = &Cache::getObject($key);
             if (is_null(self::$instances[$key]) || !is_object(self::$instances[$key]) || get_class(self::$instances[$key]) !== $class) {
                 $r = new ReflectionClass($class);
@@ -35,19 +40,37 @@ class MythBase {
 
     public function __destruct() {
         if (!is_null($this->cacheKey))
-            Cache::setObject($this->cacheKey, $this, $this->cacheLifetime);
+            Cache::setObject($this->cacheKey, &$this, $this->cacheLifetime);
+        $this->cacheKey = null;
+    }
+
+    public function __wakeup() {
+        if (!is_null($this->cacheKey)) {
+            if (isset(self::$instances[$this->cacheKey]))
+                $this->cacheKey = null;
+            else
+                self::$instances[$this->cacheKey] = &$this;
+        }
     }
 
     private static function getCacheKey($class, &$data) {
         switch ($class) {
             case 'Program':
-                $ret = array('chanid'=>$data[0]['chanid'], 'starttime'=>$data[0]['starttime']);
+                $ret = implode(',', array('chanid'=>$data[0]['chanid'], 'starttime'=>$data[0]['starttime']));
+                break;
+            case 'Channel':
+                if (isset($data['chanid']))
+                    $ret = $data['chanid'];
+                elseif (isset($data[0]))
+                    $ret = $data[0];
+                else
+                    $ret = null;
                 break;
             default:
-                $ret = $data;
+                $ret = implode(',', $data);
                 break;
         }
-        return implode(',', $ret);
+        return "$class($ret)";
     }
 
 }
