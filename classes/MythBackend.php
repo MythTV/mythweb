7f3d75e550ff0b48623acbbc931aedaa3e9ae351
John Poet <jpoet@mythtv.org>
2010-09-25 15:10:23 +0000

    Add a rsTuning state so rsRecording doesn't have to be selected before we know if the tuning succeeded or not.
    NOTE: New proto version, so a distclean is needed.
    
    Fixes #8856
    
    
    git-svn-id: http://svn.mythtv.org/svn/trunk@26518 7dbf422c-18fa-0310-86e9-fd20926502f2

--- classes/MythBackend.php
+++ classes/MythBackend.php
@@ -15,8 +15,8 @@ class MythBackend {
 
 // MYTH_PROTO_VERSION is defined in libmyth in mythtv/libs/libmyth/mythcontext.h
 // and should be the current MythTV protocol version.
-    static $protocol_version        = '62';
-    static $protocol_token          = '78B5631E';
+    static $protocol_version        = '63';
+    static $protocol_token          = '3875641D';
 
 // The character string used by the backend to separate records
     static $backend_separator       = '[]:[]';
