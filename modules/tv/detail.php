10e990ed6a13b1245205b97b3b6580f0d794f7ac
Raymond Wagner <rwagner@mythtv.org>
2011-01-13 13:32:21 -0500

    Add proper handling for JobsRunOnRecordHost flag when queuing new jobs through the Program Detail page. (cherry picked from commit 05caea0841575d00554830b5a6aee96d4a9eeef3)

--- modules/tv/detail.php
+++ modules/tv/detail.php
@@ -137,12 +137,16 @@
 
 // Queue a job?
     if ($program && $program->filename && $_REQUEST['job']) {
+        $host = "";
+        if (setting("JobsRunOnRecordHost")){
+            $host = $program->hostname;
+        }
         $db->query('INSERT INTO jobqueue
                        SET chanid       = ?,
                            starttime    = FROM_UNIXTIME(?),
                            inserttime   = NOW(),
                            type         = ?,
-                           hostname     = "",
+                           hostname     = ?,
                            args         = "",
                            status       = ?,
                            statustime   = NOW(),
@@ -152,6 +156,7 @@
                    $program->chanid,
                    $program->recstartts,
                    $_REQUEST['job'],
+                   $host,
                    JOB_QUEUED,
                    JOB_USE_CUTLIST
                   );
