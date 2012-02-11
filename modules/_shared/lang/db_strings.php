77eaf2219ab6e6ab217d1eb0e9ccc9015c45610e
Chris Petersen <cpetersen@mythtv.org>
2012-02-11 15:50:04 -0800

    Account for translation strings living in the db

---
+++ modules/_shared/lang/db_strings.php
@@ -0,0 +1,16 @@
+<?php
+/*
+ * This file is not actually run by mythweb but contains strings that 
+ * exist only in the database, so that build_translation.pl does not
+ * remove them from the translation files when it can't find them in
+ * the rest of the PHP code.
+ */
+
+# Recording filter types
+t('New episode');
+t('Identifiable episode');
+t('First showing');
+t('Primetime');
+t('Commercial free');
+t('High definition');
+t('This Episode');
