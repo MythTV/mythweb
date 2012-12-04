12ded899d73169a917127cef691ecd23d27916c3
Nicolas Riendeau <nriendeau@mythtv.org>
2012-12-03 20:51:38 -0500

    Handle the possibility of having no videos more grecefully.
    This adds a new translatable string ("No videos") that will be shown
    when there are no videos to be shown in a video folder.

--- modules/video/tmpl/default/video.php
+++ modules/video/tmpl/default/video.php
@@ -288,7 +288,14 @@
  </form>
 </td>
 <td style="text-align: right;">
- <?php echo tn('$1 video', '$1 videos', count($All_Videos)); ?>
+ <?php 
+     $video_count = count($All_Videos);
+     if( $video_count ) {
+         echo tn('$1 video', '$1 videos', $video_count); 
+     } else {
+         echo t('No videos');
+     }
+ ?>
 </td>
 </tr>
 </table>
