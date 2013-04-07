1e73a732419ff75325adbec7a971ac8ff988d63f
karog <mythtv@jgibbons.com>
2013-04-06 20:40:24 -0700

    Fixes #10600, this should fix any issues with using the os x .app with mythweb.
    Signed-off-by: Rob Smith <kormoc@mythtv.org>

--- includes/class_autoload.php
+++ includes/class_autoload.php
@@ -14,24 +14,24 @@
     function autoload($className) {
         global $Path;
         $className = str_replace('_', '/', $className);
-        if (file_exists("classes/$className.php")) {
+        if (file_exists("classes/$className.php"))
             include_once "classes/$className.php";
-            return true;
-        }
-        foreach (explode(PATH_SEPARATOR, ini_get('include_path')) as $path) {
-            if (!file_exists("$path/$className.php"))
-                continue;
-            include_once "$path/$className.php";
-            return true;
-        }
-        if (file_exists(modules_path.'/'.module."/classes/$className.php"))
+        elseif (file_exists(modules_path.'/'.module."/classes/$className.php"))
             include_once modules_path.'/'.module."/classes/$className.php";
         elseif (file_exists(modules_path.'/'.$Path[1]."/classes/$className.php"))
             include_once modules_path.'/'.$Path[1]."/classes/$className.php";
         elseif (file_exists(modules_path.'/'.$Path[0]."/classes/$className.php"))
             include_once modules_path.'/'.$Path[0]."/classes/$className.php";
-        else
+        else {
+            foreach (explode(PATH_SEPARATOR, ini_get('include_path')) as $path) {
+                if (!file_exists("$path/$className.php"))
+                    continue;
+                include_once "$path/$className.php";
+                return true;
+            }
             return false;
+        }
+        return true;
     }
 
     spl_autoload_register('autoload');
