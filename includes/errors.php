432dd4bfef619f44ba407cc0c9c9e51138c7b2e0
Nigel Jewell <nige@grufty.co.uk>
2021-12-10 22:39:00 +0000

    'ASSERT_QUIET_EVAL' constant has been removed in PHP 8.0.

--- includes/errors.php
+++ includes/errors.php
@@ -47,7 +47,7 @@
 // Active assert and make it quiet
     assert_options(ASSERT_ACTIVE,     1);
     assert_options(ASSERT_WARNING,    0);
-    assert_options(ASSERT_QUIET_EVAL, 1);
+    if (defined('ASSERT_QUIET_EVAL')) assert_options(ASSERT_QUIET_EVAL, 1);
 // Set up the callback
     assert_options(ASSERT_CALLBACK, 'assert_handler');
 
