b99922652090c1ab9db173a1d6cc4c939adfd35c
Ian Cameron <1661072+mkbloke@users.noreply.github.com>
2022-02-25 19:31:32 +0000

    Correct paths for apple-touch-icons

--- modules/_shared/tmpl/default/header.php
+++ modules/_shared/tmpl/default/header.php
@@ -50,9 +50,9 @@ EOF;
     <link rel="icon"          href="<?php echo skin_url ?>img/favicon.ico" type="image/x-icon">
     <link rel="shortcut icon" href="<?php echo skin_url ?>img/favicon.ico" type="image/x-icon">
 
-    <link rel="apple-touch-icon" href="<?php echo skin_url ?>img/apple-touch-icon.png" />
-    <link rel="apple-touch-icon" sizes="72x72" href="<?php echo skin_url ?>img/apple-touch-icon-72x72.png" />
-    <link rel="apple-touch-icon" sizes="114x114" href="<?php echo skin_url ?>img/apple-touch-icon-114x114.png" />
+    <link rel="apple-touch-icon" href="<?php echo skin_url ?>img/apple-touch-icons/apple-touch-icon.png" />
+    <link rel="apple-touch-icon" sizes="72x72" href="<?php echo skin_url ?>img/apple-touch-icons/apple-touch-icon-72x72.png" />
+    <link rel="apple-touch-icon" sizes="114x114" href="<?php echo skin_url ?>img/apple-touch-icons/apple-touch-icon-114x114.png" />
 
     <link type="application/opensearchdescription+xml" rel="search" href="tv/opensearch?type=xml" title="MythTV">
 
